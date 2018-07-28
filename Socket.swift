90c96cb6dae13742c9237cc2e16860131eb0bfcf
David Schmotz <davidschmotz@gmx.de>
2018-07-28 23:39:34 +0200

    added socket

---
+++ Socket.swift
@@ -0,0 +1,15 @@
+//
+//  Socket.swift
+//  StatMe
+//
+//  Created by David Schmotz on 19.06.18.
+//  Copyright © 2018 DavidSchmotz. All rights reserved.
+//
+
+import Foundation
+
+struct Socket {
+    var ipV6 = false
+    var ipAddress = ""
+    var port = 0
+}
